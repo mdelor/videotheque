58a4ad56afe8b94d0b1add61dc3866aff8b0fa70
mdelor <116556228+mdelor@users.noreply.github.com>
2022-11-22 14:57:59 +0100

    début vidéotheque

---
+++ js/main.js
@@ -0,0 +1,10 @@
+class Series {
+    constructor(titre, réalisateur, dureeEpisode, nombreEpisodesTotal, nombreEpisodesVues, description) {
+        this.titre = titre;
+        this.réalisateur = réalisateur;
+        this.dureeEpisode = dureeEpisode;
+        this.nombreEpisodesTotal = nombreEpisodesTotal;
+        this.nombreEpisodesVues = nombreEpisodesVues;
+        this.description = description;
+    }
+}
