ac67134083e2dfa12e6c0df06c3de9695821bc0f
mdelor <116556228+mdelor@users.noreply.github.com>
2022-12-01 10:41:30 +0100

    IMPORT API + affichage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 let films = document.getElementById("films");
 let series = document.getElementById("series");
 //on initie des compteurs pour 
@@ -12,17 +13,29 @@ let videoCompletes = 0;
 //un compteur i pour la boucle
 let i = 0;
 // boucle pour afficher chaque élément du tableau videotheque contenant les objets video
-for (let video of videotheque) {
 
-    // D'abord un switch pour identifier si on a affaire à un film, une série ou autre chose et on retourne alors une erreur.
-    switch (true) {
-        case video instanceof serie:
-            //on effectue ces instructions si on a affaire à une série.
 
-            series.insertAdjacentHTML("beforeend", `
+const movieFetch = fetch("http://localhost:3000/api/movies")
+
+let videotheque = []
+
+const afficherMovieFetch = movieFetch
+    .then(function (reponse) {
+        if (reponse.ok) {
+            return reponse.json();
+        }
+    })
+    .then(function (valeur) {
+        valeur.forEach(video => {
+            // D'abord un switch pour identifier si on a affaire à un film, une série ou autre chose et on retourne alors une erreur.
+            switch (true) {
+                case video.isSeries == true:
+                    //on effectue ces instructions si on a affaire à une série.
+
+                    series.insertAdjacentHTML("beforeend", `
 
             <div>
-                <img src="${video.logo}" alt="le logo de ${video.titre}"></img>
+                <img src="${video.imageUrl}" alt="le imageUrl de ${video.titre}"></img>
                 <ul>
                     <li>
                         <h3>${video.titre}</h3>
@@ -51,22 +64,22 @@ for (let video of videotheque) {
              </div>
 
             `);
-            //on calcule le pourcentage d'episode vue et on l'envoi vers une barre de progression
-            completion = toPercent(video.nombreEpisodesVues, video.nombreEpisodesTotal);
+                    //on calcule le pourcentage d'episode vue et on l'envoi vers une barre de progression
+                    completion = toPercent(video.nombreEpisodesVues, video.nombreEpisodesTotal);
 
-            dureeTotalVideotheque += dureeSerie(video.nombreEpisodesTotal, video.dureeEpisode);
-            dureeVueVideotheque += dureeSerie(video.nombreEpisodesVues, video.dureeEpisode);
+                    dureeTotalVideotheque += dureeSerie(video.nombreEpisodesTotal, video.dureeEpisode);
+                    dureeVueVideotheque += dureeSerie(video.nombreEpisodesVues, video.dureeEpisode);
 
-            break;
+                    break;
 
-        case video instanceof film:
-            //on effectue ces instructions si on a affaire à un film.
+                case video.isSeries == false:
+                    //on effectue ces instructions si on a affaire à un film.
 
-            completion = toPercent(video.dureeFilmVue, video.dureeFilmTotal);
-            films.insertAdjacentHTML("beforeend", `
+                    completion = toPercent(video.dureeFilmVue, video.dureeFilmTotal);
+                    films.insertAdjacentHTML("beforeend", `
             
                 <div>
-                    <img src="${video.affiche}" alt="le logo de ${video.affiche}"></img>
+                    <img src="${video.imageUrl}" alt="le imageUrl de ${video.imageUrl}"></img>
                     <div>
                         <ul>
                             <li>
@@ -97,53 +110,60 @@ for (let video of videotheque) {
 
             `);
 
-            dureeTotalVideotheque += video.dureeFilmTotal;
-            dureeVueVideotheque += video.dureeFilmVue;
+                    dureeTotalVideotheque += video.dureeFilmTotal;
+                    dureeVueVideotheque += video.dureeFilmVue;
 
-            break;
+                    break;
 
-        // par défaut on renvoie un warning dans la console et on ne fait rien d'autre.
-        default:
-            console.error("Cet objet n'est ni une série ni un film :", video)
-    }
+                // par défaut on renvoie un warning dans la console et on ne fait rien d'autre.
+                default:
+                    console.error("Cet objet n'est ni une série ni un film :", video)
+            }
 
-    let progression = document.getElementsByClassName('progression');
+            let progression = document.getElementsByClassName('progression');
 
-    //on vient ajouter des styles en fonction de la complétion
-    switch (true) {
-        //gestion erreurs
-        case (progression[i] == null):
-            console.warn("On ignore l'élément précédent", video);
-            break;
-        case (completion == 100):
-            progression[i].setAttribute("style", `width : ${completion}%; background-color: green`);
-            // on vient également compter le nombre de vidéo complétés à 100%
-            videoCompletes ++;
-            break;
-        case (completion >= 50 && completion < 100):
-            progression[i].setAttribute("style", `width : ${completion}%; background-color: orange`);
-            break;
-        case (completion > 0 && completion < 50):
-            progression[i].setAttribute("style", `width : ${completion}%; background-color: red`);
-            break;
-        default:
-            progression[i].setAttribute("style", `width : ${completion}%`);
-    }
+            //on vient ajouter des styles en fonction de la complétion
+            switch (true) {
+                //gestion erreurs
+                case (progression[i] == null):
+                    console.warn("On ignore l'élément précédent", video);
+                    break;
+                case (completion == 100):
+                    progression[i].setAttribute("style", `width : ${completion}%; background-color: green`);
+                    // on vient également compter le nombre de vidéo complétés à 100%
+                    videoCompletes++;
+                    break;
+                case (completion >= 50 && completion < 100):
+                    progression[i].setAttribute("style", `width : ${completion}%; background-color: orange`);
+                    break;
+                case (completion > 0 && completion < 50):
+                    progression[i].setAttribute("style", `width : ${completion}%; background-color: red`);
+                    break;
+                default:
+                    progression[i].setAttribute("style", `width : ${completion}%`);
+            }
 
-    //Incrémentation de notre compteur
-    i++;
+            //Incrémentation de notre compteur
+            i++;
 
-}
+        });
+        const stats = document.getElementById("stats");
+        stats.insertAdjacentHTML("beforeend", `
+            <p>Nombre total de séries et films complètement terminés : ${videoCompletes}/${i}</p>
+            <div>
+                <p>Temps total regardé : ${toHoursandMinutes(dureeVueVideotheque)}</p>
+                <p>Temps total des vidéos : ${toHoursandMinutes(dureeTotalVideotheque)}</p>
+            </div>
+        `)
+
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
+
 
 
 
 //affichage des statitistiques sur l'ensemble de la vidéothèque
 
-const stats = document.getElementById("stats");
-stats.insertAdjacentHTML("beforeend", `
-<p>Nombre total de séries et films complètement terminés : ${videoCompletes}/${i-1}</p>
-<div>
-    <p>Temps total regardé : ${toHoursandMinutes (dureeVueVideotheque)}</p>
-    <p>Temps total des vidéos : ${toHoursandMinutes (dureeTotalVideotheque)}</p>
-</div>
-`)
